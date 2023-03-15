504a4e62bbbb614e3960c2bf5ae0e5bf1aebaa6b
Mahmuthan <mahmuthanb@gmail.com>
2023-03-15 10:59:26 +0100

    Services update

--- components/Services.js
+++ components/Services.js
@@ -5,40 +5,40 @@ import ServiceItem from './ServiceItem';
 
 
 const SERVICE_DATA = [
-    {
-      serviceIcon:"/assets/images/icon/s-icon-1.png",
-      serviceTitle:"UI/UX Design",
-      serviceDesc:"Beautiful and Influential websites & apps that are mobile friendly quick to load and help drive sales giving you a solid presence online.",
-      serviceLink:"/services-details"
-    },
-    {
+  
+  {
+    serviceIcon:"/assets/images/icon/s-icon-6.png",
+    serviceTitle:"Cyber Security",
+    serviceDesc:"This service involves providing cyber security solutions to protect a client's data and systems from cyber attacks and breaches. This can include services such as network security, application security, data encryption, threat monitoring, and incident response planning.",
+    serviceLink:"/services-details"
+  },
+  {
       serviceIcon:"/assets/images/icon/s-icon-2.png",
-      serviceTitle:"Web Development",
-      serviceDesc:"Beautiful and Influential websites & apps that are mobile friendly quick to load and help drive sales giving you a solid presence online.",
+      serviceTitle:"Web Application Development w/ NextJS",
+      serviceDesc:"Next.js is framework that's React based provides many features that improve the performance and SEO of web applications, such as server-side rendering, automatic code splitting, and optimized image loading.",
       serviceLink:"/services-details"
     },
     {
       serviceIcon:"/assets/images/icon/s-icon-3.png",
-      serviceTitle:"App Development",
-      serviceDesc:"Beautiful and Influential websites & apps that are mobile friendly quick to load and help drive sales giving you a solid presence online.",
+      serviceTitle:"Mobile Application Development w/ Flutter",
+      serviceDesc:"Developing mobile applications using Flutter, a popular open-source mobile app development framework. Flutter enables developers to create cross-platform apps that run on both Android and iOS devices, using a single codebase.",
       serviceLink:"/services-details"
     },
     {
       serviceIcon:"/assets/images/icon/s-icon-4.png",
-      serviceTitle:"Digital Marketing",
-      serviceDesc:"Beautiful and Influential websites & apps that are mobile friendly quick to load and help drive sales giving you a solid presence online.",
+      serviceTitle:"Quality Assurance and Testing",
+      serviceDesc:"The process involves developing test plans, executing test cases, and identifying and reporting defects. Quality assurance and testing is essential to ensure that software functions as intended, and that it meets the needs of end-users.",
+      serviceLink:"/services-details"
+    },  {
+      serviceIcon:"/assets/images/icon/s-icon-1.png",
+      serviceTitle:"UI/UX Design",
+      serviceDesc:"Designing the user interface and user experience of software, with the goal of creating a visually appealing and user-friendly software that is easy to navigate and use. The process involves developing wireframes and prototypes to test and refine the design.",
       serviceLink:"/services-details"
     },
     {
       serviceIcon:"/assets/images/icon/s-icon-5.png",
-      serviceTitle:"Content Writing",
-      serviceDesc:"Beautiful and Influential websites & apps that are mobile friendly quick to load and help drive sales giving you a solid presence online.",
-      serviceLink:"/services-details"
-    },
-    {
-      serviceIcon:"/assets/images/icon/s-icon-6.png",
-      serviceTitle:"E-commerce",
-      serviceDesc:"Beautiful and Influential websites & apps that are mobile friendly quick to load and help drive sales giving you a solid presence online.",
+      serviceTitle:"Big Data Analytics",
+      serviceDesc:"Big data analytics can involve using various tools and technologies, such as Hadoop, Spark, and NoSQL databases, to manage and analyze large volumes of data. The service can include data integration, data mining, data visualization, and predictive analytics.",
       serviceLink:"/services-details"
     },
   ];
